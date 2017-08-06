882a86f394024ec69c6087aa7d2f8d90880cf2ce
Mario Fernandez <mario.fernandez@xing.com>
2017-08-06 21:06:40 +0200

    Fix variable substitution script

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-envsubst '$SHELF_HOST' < /tmp/shelf.conf.template > /etc/nginx/sites-enabled/shelf.conf
-envsubst '$SHELF2_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
+envsubst '$$SHELF_HOST' < /tmp/shelf.conf.template > /etc/nginx/sites-enabled/shelf.conf
+envsubst '$$SHELF2_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
 
 nginx -g 'daemon off;'
