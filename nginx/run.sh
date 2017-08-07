cd3fd5c4b79111f249a98664a3c782dd179c4f3a
Mario Fernandez <mario.fernandez@xing.com>
2017-08-07 21:54:13 +0200

    Add config for images subdomain

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 envsubst '$$HOST' < /tmp/main.conf.template > /etc/nginx/sites-enabled/main.conf
+envsubst '$$HOST' < /tmp/images.conf.template > /etc/nginx/sites-enabled/images.conf
 envsubst '$$SHELF2_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
 
 nginx -g 'daemon off;'
