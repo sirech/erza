35dd50c952f4897421649188922722b594916721
Mario Fernandez <mario.fernandez@xing.com>
2017-08-07 20:39:15 +0200

    Add config for main domain

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+envsubst '$$HOST' < /tmp/main.conf.template > /etc/nginx/sites-enabled/main.conf
 envsubst '$$SHELF2_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
 
 nginx -g 'daemon off;'
