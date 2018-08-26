a02505fe5268bebf5797ec60c247e9303bb193b5
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-08-26 17:39:24 +0200

    Shelf2 frontend is no longer deployed through docker

--- spec/erza/folders_spec.rb
+++ spec/erza/folders_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
-describe file('/srv/main') do
-  it { is_expected.to be_directory }
-  it { is_expected.to be_owned_by('travis') }
+%i[main shelf2_public].each do |folder|
+  describe file("/srv/#{folder}") do
+    it { is_expected.to be_directory }
+    it { is_expected.to be_owned_by('travis') }
+  end
 end
