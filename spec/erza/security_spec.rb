0e77ed2d7ef2a01066546a663738fc3f50492c61
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-05-02 22:05:24 +0200

    don't expose non nginx ports

--- spec/erza/security_spec.rb
+++ spec/erza/security_spec.rb
@@ -11,3 +11,15 @@ end
 describe cron do
   it { is_expected.to have_entry '0 6 1 * * sh /srv/erza/bin/renew 2>&1 | /usr/bin/logger -t letsencrypt' }
 end
+
+[22, 80, 443].each do |port|
+  describe port(port) do
+    it { is_expected.to be_listening }
+  end
+end
+
+[3000, 3306, 4000].each do |port|
+  describe port(port) do
+    it { is_expected.not_to be_listening }
+  end
+end
