39793c6274bcc9ebea5826c02344f831761ef2cc
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-07-27 19:34:36 +0200

    don't use bash with alpine

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ash
 envsubst '$$HOST' < /tmp/main.conf.template > /etc/nginx/sites-enabled/main.conf
 envsubst '$$HOST' < /tmp/images.conf.template > /etc/nginx/sites-enabled/images.conf
 envsubst '$$SHELF2_HOST $$AUTH0_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
