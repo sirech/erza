3aa4ab92e2401b025d86c2242e4cd7787fad9b62
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-07-27 15:02:16 +0200

    Add CSP policy for shelf

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 envsubst '$$HOST' < /tmp/main.conf.template > /etc/nginx/sites-enabled/main.conf
 envsubst '$$HOST' < /tmp/images.conf.template > /etc/nginx/sites-enabled/images.conf
-envsubst '$$SHELF2_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
+envsubst '$$SHELF2_HOST $$AUTH0_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
 envsubst '$$ECHO_HOST' < /tmp/echo.conf.template > /etc/nginx/sites-enabled/echo.conf
 
 nginx -g 'daemon off;'
