c0c04b4d075c0e1adb1ef48f25aeb3f4112fddcc
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-24 14:50:26 +0200

    add checks for cookery

--- spec/erza/folders_spec.rb
+++ spec/erza/folders_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-%i[main shelf2_public].each do |folder|
+%i[main shelf2_public cookery2_public].each do |folder|
   describe file("/srv/#{folder}") do
     it { is_expected.to be_directory }
     it { is_expected.to be_owned_by('travis') }
