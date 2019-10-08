025745d39f4b121eac4f07f3ad08bee6a2fb7791
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-10-08 13:05:52 +0200

    add alternate domain for http

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env ash
-envsubst '$$HOST' < /tmp/main.conf.template > /etc/nginx/sites-enabled/main.conf
+envsubst '$$HOST $$ALTERNATIVE_HOST' < /tmp/main.conf.template > /etc/nginx/sites-enabled/main.conf
 envsubst '$$HOST' < /tmp/images.conf.template > /etc/nginx/sites-enabled/images.conf
 envsubst '$$SHELF2_HOST $$AUTH0_HOST' < /tmp/shelf2.conf.template > /etc/nginx/sites-enabled/shelf2.conf
 envsubst '$$COOKERY2_HOST $$AUTH0_HOST' < /tmp/cookery2.conf.template > /etc/nginx/sites-enabled/cookery2.conf
