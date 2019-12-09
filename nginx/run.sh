c16f7588f6e4b9d4bf939fe08a4572e87bd4b9c9
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-12-10 00:53:12 +0100

    extract acme-challenge to fragment

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env ash
 
-gomplate --input-dir /tmp/templates --output-map='/etc/nginx/sites-enabled/{{ .in | strings.ReplaceAll ".conf.tmpl" ".conf" }}'
+gomplate \
+    -t acme-challenge=/tmp/fragments/acme-challenge.t \
+    --input-dir /tmp/templates \
+    --output-map='/etc/nginx/sites-enabled/{{ .in | strings.ReplaceAll ".conf.tmpl" ".conf" }}'
 nginx -g 'daemon off;'
