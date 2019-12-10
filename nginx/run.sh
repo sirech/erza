3ddf8845ceb860bc684b3b48b6ab478a541db2c0
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-12-10 10:53:48 +0100

    extract ssl configuration into fragment

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,6 +2,7 @@
 
 gomplate \
     -t acme-challenge=/tmp/fragments/acme-challenge.t \
+    -t ssl-enabled=/tmp/fragments/ssl-enabled.t \
     --input-dir /tmp/templates \
     --output-map='/etc/nginx/sites-enabled/{{ .in | strings.ReplaceAll ".conf.tmpl" ".conf" }}'
 nginx -g 'daemon off;'
