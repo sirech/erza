818af96fe9c63839c851893563f3c5e413c208a1
Mario Fernandez <mario.fernandez@hceris.com>
2020-06-30 19:29:45 +0200

    extract certificate tests into own folder

--- spec/erza/security_spec.rb
+++ spec/erza/security_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
-%w[cert cert/live cert/renewal].each do |file|
-  describe file("/#{file}") do
-    it { is_expected.to be_directory }
-    it { is_expected.to be_readable }
-    it { is_expected.to be_directory }
-  end
-end
-
-describe cron do
-  it { is_expected.to have_entry '0 6 * * 1 sh /srv/erza/bin/renew 2>&1 | /usr/bin/logger -t letsencrypt' }
-end
-
 [22, 80, 443].each do |port|
   describe port(port) do
     it { is_expected.to be_listening }
