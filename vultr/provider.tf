7c795d9b18dfd068ae1b80a682e625c64618a0a0
Mario Fernandez <mario.fernandez@hceris.com>
2020-12-15 21:16:30 +0100

    update to tf 0.13

--- vultr/provider.tf
+++ vultr/provider.tf
@@ -1,6 +1,13 @@
-provider "vultr" {
-  version = "~> 2.0.0"
+terraform {
+  required_providers {
+    vultr = {
+      source  = "vultr/vultr"
+      version = "~> 2.0.0"
+    }
+  }
+}
 
+provider "vultr" {
   api_key    = var.vultr_api_key
   rate_limit = 2000
 }
