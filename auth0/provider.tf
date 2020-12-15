28f7fe2bf1f6caa368926c17fc0afce1768dfe0d
Mario Fernandez <mario.fernandez@hceris.com>
2020-12-15 21:20:39 +0100

    update to tf 0.13

--- auth0/provider.tf
+++ auth0/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "alexkappa/auth0"
+      version = "~> 0.16.0"
+    }
+  }
+}
+
 provider "auth0" {
   domain        = var.auth0_domain
   client_id     = var.auth0_client_id
