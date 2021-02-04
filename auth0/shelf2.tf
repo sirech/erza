7242807aeb9ea14f03970660033ed4e8d53fe6d5
Mario Fernandez <mario.fernandez@hceris.com>
2021-02-04 15:28:31 +0100

    switch shelf2 back to correct port again

--- auth0/shelf2.tf
+++ auth0/shelf2.tf
@@ -2,8 +2,8 @@ resource "auth0_client" "shelf2-frontend" {
   name        = "shelf2-frontend"
   description = "Shelf2 Application - Terraform generated"
   app_type    = "spa"
-  callbacks   = ["http://localhost:9090/callback", "https://${local.shelf2_host}/callback"]
-  web_origins = ["http://localhost:9090", "https://${local.shelf2_host}"]
+  callbacks   = ["http://localhost:3000/callback", "https://${local.shelf2_host}/callback"]
+  web_origins = ["http://localhost:3000", "https://${local.shelf2_host}"]
 
   oidc_conformant = true
 
